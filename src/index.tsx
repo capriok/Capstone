cafe709af71cb3952f4c5e9bb22024ca6697bac6
capriok <capriok7@gmail.com>
2022-01-27 09:53:02 -0700

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import App from './app'
 
 ReactDOM.render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <Route path="/">
+        <App />
+      </Route>
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 )
